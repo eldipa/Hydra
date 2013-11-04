69251cda41b69da2224e8ec881da8ccfc7ea59a1
Di Tomaso Nicolas <ditomasonicolas@gmail.com>
2013-11-04 06:01:29 +0000

    Comienzos de locura intralenguajes

---
+++ test_nico/c_code/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Cpp_Project.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola mundo C++!!" << endl;
+	return 0;
+}
---
+++ test_nico/p_code/main.py
@@ -0,0 +1,5 @@
+import subprocess
+
+print 'Hola mundo'
+print 'Ejecutando codigo c++:'
+subprocess.call(['../c_code/Debug/Cpp_Code'])
