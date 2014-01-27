b72d1ff586c4e7d0ca20d1dedc3c599ef9a65fdb
Di Paola Martin <martinp.dipaola@gmail.com>
2014-01-27 01:41:17 +0000

    Toques finales. Por ahora son solo unos scripts pero es suficiente.

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,7 @@
-zip -r concu-debug.nw package.json nw-main.html
+echo "Compressing..."
+zip -r concu-debug.nw package.json nw-main.html 
+echo "Building..."
+cat ../test_nico/NodeWebkitTest/NodeWebkit/node-webkit-v0.8.4-linux-ia32/nw concu-debug.nw > concu-debug
+chmod u+x  concu-debug
+rm concu-debug.nw
+echo "Done"
--- src/start.sh
+++ src/start.sh
@@ -1,1 +1,3 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/google/chrome/
+./concu-debug > /dev/null 2>&1 &
 python -m bottle --debug --reload main 2>&1 | python colored.py
