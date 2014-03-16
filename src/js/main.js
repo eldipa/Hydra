a612e9254743e776a1aaa04f6b7d3ab75eda80f4
Di Paola Martin <martinp.dipaola@gmail.com>
2014-03-16 14:48:53 +0000

    Vista de grafo de procesos.

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,70 @@ requirejs.config({
 
 });
 
-requirejs(['d3', 'ko'], function (d3, ko) {
-   console.log(d3.version);
-   console.log(ko.version);
-
+requirejs(['d3', 'ko', 'process_graph'], function (d3, ko, pgraph) {
    var fs = require('fs');
-    
-   fs.readFile('./package.json', 'utf-8', function (error, contents) {
-          //document.write(contents);
-          console.log(contents);
-   });
 
-   var f = require('./js/f.js');
+   console.log(d3.select('.main'));
+   var main = d3.select('.main')
+      .append('div')
+      .attr('class', 'col-xs-12 col-sm-9');
+
+   console.log(main);
+
+
+   var form = d3.select('.main')
+      .append('div')
+      .attr('class', 'col-xs-6 col-sm-3 sidebar-offcanvas')
+      .attr('role', 'navigation')
+      .append('div')
+      .attr('class', 'form-group');
+
+   form.append('label')
+      .text('View type')
+   var s = form.append('select')
+      .attr('class', 'form-control')
+      .attr('data-bind', 'value: view_type');
+   s.append('option').text('graph');
+   s.append('option').text('table');
+
+   form.append('label')
+      .text('Include all descendents');
+   form.append('input')
+      .attr('type', 'checkbox')
+      .attr('data-bind', 'checked: include_all_descendents');
+
+   form.append('label')
+      .text('Polling');
+   form.append('input')
+      .attr('type', 'text')
+      .attr('class', 'form-control')
+      .attr('placeholder', 'Polling time')
+      .attr('data-bind', 'value: polling');
+
+   var v = new pgraph.ProcessGraph();
+   v.enable(main);
+   v.update([
+         {
+            pid: 1, 
+            name: 'A',
+            status: 'running'
+         },
+         {
+            pid: 2, 
+            name: 'B',
+            status: 'running'
+         },
+         {
+            pid: 3, 
+            name: 'C',
+            status: 'running'
+         }
+         ],
+
+         [
+         [0, 1],
+         [0, 2]
+            ]);
 
 },
 function (err) {
---
+++ src/js/process_graph.js
@@ -0,0 +1,259 @@
+define(['d3'], function (d3) {
+   var ProcessGraph = function () {
+      function ProcessGraph(width, height) {
+         this.width = (typeof width === 'undefined') ? 600 : width;
+         this.height = (typeof height === 'undefined') ? 400 : height;
+
+         this.graph = d3.layout.force()
+            .size([this.width, this.height])
+            .linkDistance(100)
+            .charge(-600);
+
+         this.stop_update_graph_on_level = 0.03;
+         
+         this.color_by_status = {
+            "running": '#0f0',
+            "sleeping": 'rgb(33, 184, 190)',
+            "disk-sleep": 'rgb(190, 71, 33)',
+            "stopped": '#f00',
+            "tracing-stop": '#f00',
+            "zombie": 'rgb(165, 111, 111)',
+            "dead": '#fff',
+            "wake-kill": 'rgb(111, 165, 120)',
+            "waking": 'rgb(111, 165, 120)',
+            "idle": '#00f',
+            "locked":'#00f',
+            "waiting":'#00f'
+         };
+      }
+
+      ProcessGraph.prototype.enable = function (container_to_attach) {
+         if (typeof this.svg !== 'undefined') {
+            this.disable();
+         }
+
+         this.svg = container_to_attach.append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height);
+
+         this.svg.append("rect")
+            .attr("width", this.width)
+            .attr("height", this.height)
+            .attr('fill', 'none');
+         
+         var self = this; 
+         this.graph.on("tick", function () { 
+            return self.update_graph_one_round(); });
+      };
+
+      ProcessGraph.prototype.disable = function () {
+         if (typeof this.svg !== 'undefined') {
+            this.svg.remove();
+            delete this.svg;
+         }
+      };
+
+      ProcessGraph.prototype.update_graph_one_round = function () {
+         console.log('tick');
+         if (this.graph.alpha() < this.stop_update_graph_on_level) {
+            this.graph.alpha(0);
+         }
+      
+         // update links
+         d3.selectAll('.link')
+            .attr("x1", function (d) { return d.source.x; } )
+            .attr("y1", function (d) { return d.source.y; } )
+            .attr("x2", function (d) { return d.target.x; } )
+            .attr("y2", function (d) { return d.target.y; } );
+
+         // update nodes
+         var self = this;
+         d3.selectAll('.circle_node')
+            .attr("cx", function (process) { return process.x; } )
+            .attr("cy", function (process) { return process.y; } )
+            .attr('fill', function (process) { return self.color_by_status[process.status];} );
+
+         // update the text of each node
+         d3.selectAll('.node_text')
+            .attr('x', function (process) { return process.x; })
+            .attr('y', function (process) { return process.y; })
+            .text(function (process) { return process.pid + " " + process.name;});
+      };
+
+      ProcessGraph.prototype.update_graph_view = function (restart) {
+         var nodes = this.svg.selectAll('.node')
+            .data(this.graph.nodes(), function (gprocess) { return gprocess.pid; });
+
+         nodes.exit()
+            .remove()
+
+         var gnodes = nodes.enter()
+            .append('g')
+            .attr('class', 'node');
+
+         gnodes.append('circle')
+            .attr('class', 'circle_node')
+            .attr('r', 6)
+            .call(this.graph.drag);
+         gnodes.append('text')
+            .attr('class', 'node_text');
+
+         var links = this.svg.selectAll('.link').data(this.graph.links());
+
+         links.enter()
+            .append('line')
+            .attr('class', 'link')
+            .attr('stroke', '#999');
+
+         links.exit()
+            .remove();
+
+         if (restart) {
+            this.graph.start();
+         }
+         else {
+            var self = this;
+            (function () { self.update_graph_one_round(); })();
+         }
+      };
+
+      ProcessGraph.prototype.update_graph_data = function (processes, relations) {
+         var pids = [];
+         processes.forEach(function (p) { pids.push(p);}, this);
+         var graph_modified = false;
+
+         // remove old processes
+         var to_remove = [];
+         this.graph.nodes().forEach(function (n) {
+            if(n.p && pids.indexOf(n.p.pid) < 0) {
+               to_remove.push(n.i);
+            }
+         }, this);
+
+         if (to_remove.length) {
+            graph_modified = true;
+         }
+
+         var offset = 0;
+         to_remove.forEach(function (i) { this.graph.nodes().splice(i-offset, 1); offset += 1; }, this);
+         
+         // update processes
+         processes.forEach(function (p) {
+            this.graph.nodes().every(function (n) {
+               var gp = n.p;
+               var i = n.i;
+
+               if (gp.pid === p.pid) {
+                  for (attr in p) {
+                     this.graph.nodes()[i][attr] = p[attr];
+                  }
+                  return false;
+               }
+               return true;
+            }, this);
+         }, this);
+
+         // add new processes
+         var gpids = [];
+         this.graph.nodes().forEach(function (gp) { gpids.push(gp.pid); }, this); 
+         var to_add = [];
+         processes.forEach(function (p) {
+            if (gpids.indexOf(p.pid) < 0) {
+               to_add.push(p);
+            }
+         }, this);
+         if (to_add.length) {
+            graph_modified = true;
+         }
+         to_add.forEach(function (p) {
+            this.graph.nodes().push(p);
+         }, this);
+         
+         // remove old links
+         var to_remove = [];
+         this.graph.links().forEach(function(link) {
+            var l = link.l;
+            var i = link.i;
+            var found = false;
+
+            relations.every(function (rel) {
+               if(l.source.pid === processes[rel[o]].pid && l.target.pid === processes[rel[1]].pid) {
+                  found = true;
+                  return false;
+               }
+               return true;
+            }, this);
+
+            if(!found) {
+               to_remove.push(i);
+            }
+         }, this);
+
+         var offset = 0;
+         if(to_remove.length) {
+            graph_modified = true;
+         }
+         to_remove.forEach(function (i) {
+            this.graph.links().splice(i-offset, 1);
+            offset += 1;
+         }, this);
+
+         
+         // add new links
+         var to_add = [];
+         relations.forEach(function (rel) {
+            var found = false;
+            this.graph.links().every(function (l) {
+               if (l.source.pid === processes[rel[0]].pid && l.target.pid === processes[rel[1]].pid) {
+                  found = true;
+                  return false;
+               }
+               return true;
+            }, this);
+
+            if(!found) {
+               var source = null;
+               var target = null;
+
+               this.graph.nodes().every(function (p) {
+                  if(p){
+                     if(p.pid === processes[rel[0]].pid) {
+                        source = p;
+                     }
+                     else if(p.pid === processes[rel[1]].pid) {
+                        target = p;
+                     }
+                     
+                     if(source !== null && target !== null) {
+                        return false;
+                     }
+                  }
+                  return true;
+               }, this);
+
+               to_add.push({source: source, target: target});
+            }
+         }, this);
+         
+         if (to_add.length) {
+            graph_modified = true;
+         }
+         to_add.forEach(function (l) {
+            this.graph.links().push(l);
+         }, this);
+
+         return graph_modified;
+      };
+
+      ProcessGraph.prototype.update = function (processes, relations) {
+         if (this.svg) {
+            var changed_graph_or_link_count = this.update_graph_data(processes, relations);
+            this.update_graph_view(changed_graph_or_link_count);
+         }
+      };
+
+      return ProcessGraph;
+   }();
+
+   return {ProcessGraph: ProcessGraph};
+});
