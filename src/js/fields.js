7181665224998052d6e0d530278ae9aa17f7cab0
Di Paola Martin <martinp.dipaola@gmail.com>
2014-03-23 04:14:04 +0000

    Mas widgets.

--- src/js/fields.js
+++ src/js/fields.js
@@ -1,5 +1,5 @@
 define(['d3', 'ko'], function(d3, ko) {
-   var view_it = function (model, container_to_attach) {
+   var view_it = function (model, container_to_attach, horizontal_form) {
       
       if(!model.fieldsets) {
          throw "The attribute fieldsets must not be '" + model.fieldsets + "'.";
@@ -17,55 +17,71 @@ define(['d3', 'ko'], function(d3, ko) {
        *  - each "field" in the array is an object like = {
        *                            name: ... field name
        *                            widget: ... how to represent the field
-       *                            multifield: ... optional boolean
        *                            }
        *
-       *  - if the multifield is True, the name and the widget must be arrays
-       *    instead of simple elements.
-       *    The list of name-widget tuples will be displayed as normals fields
-       *    but all in the same line.
-       *    The default is multifield = False
        *
        *  - then, each field name must be an attribute of the model and
        *    must be (or should be?) an ko.observable object.
        *
        **/
 
-      var fieldsets = d3.select(container_to_attach).selectAll('.fieldset').data(model.fieldsets);
+      // fieldsets bind
+      var root = d3.select(container_to_attach);
+      var label_class = "control-label"
+      if(horizontal_form) {
+         root = root.append('form').attr('class', 'form-horizontal');
+         label_class = label_class + " " + "col-sm-2";
+      }
+      var fieldsets = root.selectAll('.fieldset').data(model.fieldsets);
 
+      // fieldsets enter()
+      // div
+      //  -- header (name of the set)
+      //      -- small (description)
       var fieldset_divs = fieldsets.enter()
          .append('div')
          .attr('class', function (fset) {
-            return "fieldset" + " " + fset.classes;   //TODO only if it is not empty and defined
-         }); //TODO add more bootstrap class
+            return "fieldset" + " " + (fset.classes || "");
+         }); 
 
-      fieldset_divs.append('h3')
+      var set_heads = fieldset_divs.append('h4')
+         .attr('style', function (fset) { 
+            if(fset.name) {
+               return null;
+            }
+            else {
+               return 'display:none';
+            }
+         })
          .text(function (fset) { 
-            return fset.name;
-         }); //TODO add more attrs
+            return fset.name || "";
+         }); 
 
-      fieldset_divs.append('h4')
-         .text(function (fset) {
-            return fset.description;   //TODO only if it is not undefined
-         }); //TODO more attrs
+      set_heads.append('small')
+         .attr('style', function (fset) { 
+            if(fset.description) {
+               return null;
+            }
+            else {
+               return 'display:none';
+            }
+         })
+         .text(function (fset) { 
+            return " " + (fset.description || "");
+         }); 
 
 
+      // fieldsets exit()
       fieldsets.exit()
          .remove();
 
+      // fieldsets done, now each field is created (bind)
       var fields = fieldsets.selectAll('.form-group').data(function (fset) { 
          return fset.fields;
       });
 
-      /*
-         if(fset.multifield) {
-            return d3.zip(fset.name, fset.widget);
-         }
-         else {
-            return [[fset.name, fset.widget]];
-         }
-         */
 
+      // fields enter() and exit()
       fields.enter()
          .append('div')
          .attr('class', 'form-group');
@@ -74,9 +90,83 @@ define(['d3', 'ko'], function(d3, ko) {
          .remove();
 
 
-      fields.append('label').text(function (field) { return field.name; });
-      fields.append(function (field) { return document.createElement(field.widget.tag); })
-         .attr('planceholder', function (field) { return field.name; }); //TODO add more attrs for bootstrap, and ko
+      fields.append('label').text(function (field) { return field.name; })
+         .attr('class', label_class);
+
+      if(horizontal_form) {
+         fields = fields.append('div').attr('class', 'col-sm-10');
+      }
+
+      // field creation (except checkboxs)
+      var non_checkbox_packets = fields.filter(function (field) {
+         return field.widget.type !== "checkbox";
+      })
+         .append(function (field) { 
+            var tag = "input";
+            var type = field.widget.type;
+            if(type === "textarea") {
+               tag = "textarea";
+            }
+            else if (type === "static") {
+               tag = "p";
+            }
+            else if (type === "select" || type === "multiselect") {
+               tag = "select";
+            }
+
+            return document.createElement(tag); 
+         })
+         .attr('placeholder', function (field) { return field.widget.placeholder || null; })
+         .attr('type', function (field) { return field.widget.type || null})
+         .attr('class', function (field) {
+            if(field.widget.type === "static") {
+               return 'form-control-static';
+            } else {
+               return 'form-control';
+            }
+         })
+         .attr('multiple', function (field) { 
+            if(field.widget.type === "multiselect") {
+               return "";
+            } else {
+               return null;
+            }
+         })
+         .text(function (field) { return field.widget.message || "";}); //TODO add more attrs for bootstrap, and ko
+
+      // special field creation (checkboxs)
+      var checkbox_packets = fields.filter(function (field) {
+         return field.widget.type === "checkbox";
+      })
+         .append('div')
+         .attr('class', 'checkbox');
+
+      checkbox_packets.html(function (field) { return '<input type="checkbox"> ' + (field.widget.message || "");});
+
+      // special children (options)
+      var options = non_checkbox_packets.filter(function (field) { return field.widget.type === "select" || field.widget.type === "multiselect"; })
+         .selectAll('option')
+         .data(function (field) { return field.widget.options || []; });
+
+      options.enter()
+         .append('option');
+
+      options.exit()
+         .remove()
+
+      options.text(function (opt) { return opt; });
+
+      fields.append('p').text(function (field) { return field.widget.help || ""; })
+         .attr('class', 'help-block')
+         .attr('style', function (field) { 
+            if(field.widget.help) {
+               return null;
+            }
+            else {
+               return 'display:none';
+            }
+         });
+
 
    };
 
--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,66 @@ requirejs.config({
 
 });
 
-requirejs(['d3', 'ko', 'process_view'], function (d3, ko, process_view) {
+requirejs(['d3', 'ko', 'process_view', 'fields'], function (d3, ko, process_view, fields) {
    var fs = require('fs');
    
-   process_view.start();
+   //process_view.start();
+   //require('nw.gui').Window.get().reload(3);
+   var model = {
+      fieldsets: [
+         {
+            name: "Foo category",
+            description: "Example of fields.",
+            fields: [
+                {
+                   name: "Name:",
+                   widget: {type: 'text', placeholder: 'Your name here'}
+                },
+                {
+                   name: "Password:",
+                   widget: {type: 'password', help: "use lower and upper letters with numbers and signs"}
+                },
+                {
+                   name: "Your opinion:",
+                   widget: {type: 'textarea'}
+                },
+                {
+                   widget: {type: 'checkbox', help:'beware!!!', message:"yes?"}
+                },
+                {
+                   name: "Your money:",
+                   widget: {type: 'static', message:"1244$", help:"it's constant :)"}
+                },
+                {
+                   name: "Select one:",
+                   widget: {type: 'select', options: [1, 2]}
+                },
+                {
+                   name: "Select more!:",
+                   widget: {type: 'multiselect', help:"use CTRL to select more than one", options: [1, 2, 3, 4]}
+                },
+               ]
+         },
+         
+         {
+            name: 'Campos B',
+            description: "foooo b",
+            classes: '',
+            fields: [
+                {
+                   name: "Nombre:",
+                   widget: {tag: 'input'}
+                },
+                {
+                   name: "Apellido:",
+                   widget: {tag: 'input'}
+                },
+               ]
+         }
+         
+         ]
+   };
+   fields.view_it(model, '.main', true);
 
 
    /*
