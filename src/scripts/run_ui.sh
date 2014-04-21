ef8f6d98dd139b2dcec655d835d92abfd3c09658
Di Paola Martin <martinp.dipaola@gmail.com>
2014-04-21 00:05:31 +0000

    Agregadas unas notas a run_ui.sh

--- src/scripts/run_ui.sh
+++ src/scripts/run_ui.sh
@@ -1,3 +1,6 @@
+#./concu-debug > /dev/null 2>&1 &
+#python -m bottle --debug --reload main 2>&1 | python colored.py
+#../NodeWebkit/node-webkit-v0.8.4-linux-ia32/nw ./bin/concu-debug.nw
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/google/chrome/
 trap "killall nw; kill 0" EXIT
 ../NodeWebkit/node-webkit-v0.8.4-linux-ia32/nw .
