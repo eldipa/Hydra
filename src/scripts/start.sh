066aae9bfa93c4b4894cfdb88d47d9128bf99f46
Di Paola Martin <martinp.dipaola@gmail.com>
2014-04-21 00:05:22 +0000

    Simplificado el manejo de errores en el script start.sh.

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,9 +1,5 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/google/chrome/
-#./concu-debug > /dev/null 2>&1 &
-#python -m bottle --debug --reload main 2>&1 | python colored.py
-#../NodeWebkit/node-webkit-v0.8.4-linux-ia32/nw ./bin/concu-debug.nw
+trap "killall nw; kill 0" EXIT
+
 touch update
 ./scripts/keep_refreshing.sh &
-PID=$!
-trap "kill -15 $PID" SIGINT SIGTERM
-../NodeWebkit/node-webkit-v0.8.4-linux-ia32/nw . 
+./scripts/run_ui.sh
