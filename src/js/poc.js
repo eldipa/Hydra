98e2d3649fef498c7cfd645e921e5dc8bad71927
Di Paola Martin <martinp.dipaola@gmail.com>
2014-06-01 23:34:07 -0300

    Mas habilidades al poc. Puede cargar un file y setear la linea actual asi como tambien mostrar mensajes provenientes de gdb.

--- src/js/poc.js
+++ src/js/poc.js
@@ -27,28 +27,33 @@ define(['w2ui', 'code_view', 'event_handler', 'jquery'], function (w2ui, code_vi
 
    event_handler.subscribe('debugger.new-session', function (data) {
       session_id = data - 0;
-      event_handler.publish(session_id + ".run", "");
-   });
 
-   event_handler.subscribe('debugger.new-target', function (data) {
-      console.log("Event: " + JSON.stringify(data));
-      var received_session = data.gdbPid - 0;
-      if (received_session !== session_id) {
-         console.log("ERROR: actual session '"+session_id+"' != the received session '"+received_session+"' in the new-target event!");
-      }
+      event_handler.subscribe('debugger.new-target', function (data) {
+         console.log("Event: " + JSON.stringify(data));
+         var received_session = data.gdbPid - 0;
+         if (received_session !== session_id) {
+            console.log("ERROR: actual session '"+session_id+"' != the received session '"+received_session+"' in the new-target event!");
+         }
 
-      target_id = data.targetPid - 0;
-      console.log("Target: " + target_id);
+         target_id = data.targetPid - 0;
+         console.log("Target: " + target_id);
+
+         //event_handler.publish(session_id+".direct-command", "");
+      });
+
 
       event_handler.subscribe("pid."+session_id, function (data) {
-         console.log("Event: " + JSON.stringify(data));
          if (data.klass === "stopped" && data.type === "Exec") {
             if (data.results && data.results.reason === "exited-normally") {
                // TODO end
             }
 
-            if (data.results && data.results.fram && data.results.frame.line && data.results.frame.file) {
-               // TODO load file and line
+            if (data.results && data.results.frame && data.results.frame.line && data.results.frame.fullname) {
+               console.log("Event: " + JSON.stringify(data));
+               console.log("Fullname: " + data.results.frame.fullname);
+               // load file and line
+               view.load_code_from_file(data.results.frame.fullname);
+               view.gotoLine(data.results.frame.line - 0);
             }
          }
       });
@@ -56,9 +61,17 @@ define(['w2ui', 'code_view', 'event_handler', 'jquery'], function (w2ui, code_vi
       event_handler.subscribe("pid."+session_id, function (data) {
          if (data.type === "Console") {
             var old = w2ui.objects['layout'].content("bottom");
-            w2ui.objects['layout'].content("bottom", old + data.stream);
+            w2ui.objects['layout'].content("bottom", old + data.stream + "<br />");
          }
       });
+
+      event_handler.publish(session_id + ".direct-command", "start");
+      //event_handler.publish(session_id + ".direct-command", "b main");
+      //event_handler.publish(session_id + ".run", "");
+
+      setTimeout(function () {
+         event_handler.publish(session_id + ".direct-command", "c");
+      }, 30000);
    });
 
    event_handler.publish("debugger.load", "cppTestCode/testExe");
