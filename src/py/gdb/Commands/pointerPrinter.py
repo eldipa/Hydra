36acf3a728f24d2519d0b9e29bbed6babdf4b25d
NicolasDiTomaso <NicolasDiTomaso@users.noreply.github.com>
2014-06-13 04:42:48 -0300

    Comando adicional a gdb hecho en python que imprime todos los valores de los puntero pasados como argumento en una sola linea en formato [val,val]. Aun queda el problema que gdbmi lo lanza por stream en vez de record issue #23

---
+++ src/py/gdb/Commands/pointerPrinter.py
@@ -0,0 +1,36 @@
+'''
+Created on 12/06/2014
+
+@author: nicolas
+'''
+
+import gdb
+import StringIO
+
+# Modo de uso:
+# (gdb) python execfile ("../py/gdb/Commands/pointerPrinter.py")
+# (gdb) pointer-printer punteroEntero 0xbffff25c
+
+class CommandPointerPrinter(gdb.Command):
+
+    def __init__(self):
+        super(CommandPointerPrinter, self).__init__('pointer-printer', gdb.COMMAND_DATA)
+
+    def invoke (self , args , from_tty) :
+        
+        argv = gdb.string_to_argv(args)
+    
+        output = StringIO.StringIO()
+        print >> output, "[",
+        print >> output, gdb.parse_and_eval('*' + argv[0]),
+        
+        for i in range(1,len(argv)):
+            print >> output, ",",
+            print >> output, gdb.parse_and_eval('*' + argv[i]),
+            
+        print >> output, "]",
+        resultado = output.getvalue()
+        print(resultado.replace(" ", ''))
+        output.close()
+
+CommandPointerPrinter()
