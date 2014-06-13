2ddcf524153ec08674db1110802ed6f36ca18d69
NicolasDiTomaso <NicolasDiTomaso@users.noreply.github.com>
2014-06-12 22:41:56 -0300

    Cambios en el main. Ahora si se llama desde una ubicacion incorrecta retorna error, elimina la cola en el finally y acepta q para salir

--- src/py/main.py
+++ src/py/main.py
@@ -7,31 +7,31 @@ Created on 19/04/2014
 import gdb.gdbSpawmer
 import subprocess
 import os
+import sys
 from sys import stdin
+from re import match
 
-# # ESTE MAIN SE DEBE LLAMAR DESDE LA CARPETA SRC
-
+if (not match(".*/src$", os.getcwd())):
+    print "ESTE MAIN SE DEBE LLAMAR DESDE LA CARPETA SRC"
+    sys.exit()
 
 try:
-    
-    
-
 
     # lanzo nw
     ui_process = subprocess.Popen(['./scripts/run_ui.sh'], shell=True)
-
 
     os.system("python py/publish_subscribe/billboard.py start")
 
     spawmer = gdb.gdbSpawmer.GdbSpawmer()
 
     # esperar quit
-    while(stdin.readline() != "quit\n"):
-        print "Entrada invalida, ingrese quit para salir"
-        
-    spawmer.eliminarCola()
+    while(stdin.readline() not in ["quit\n","q\n"]):
+        print "Entrada invalida, ingrese quit o q para salir"
 
 finally:
+    
+    spawmer.eliminarCola()
+    
     os.system("python py/publish_subscribe/billboard.py stop")
  
     if ui_process:
