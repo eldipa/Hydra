dc5232af9fddc222d546de5293733916e98b22a8
Di Paola Martin <martinp.dipaola@gmail.com>
2014-09-13 20:42:54 -0300

    Removido codigo no usado en main.js

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,6 @@ requirejs(['w2ui', 'code_view', 'jquery', 'export_console', 'layout', 'layout_ex
 
    ctxmenu.init({
           fadeSpeed: 100,
-          filter: function ($obj){},
           above: 'auto',
           preventDoubleContext: true,
           compress: false
