ab14a2f5a43591371e48f071563e505b2764fef8
Di Paola Martin <martinp.dipaola@gmail.com>
2014-10-25 18:58:13 -0300

    Implementados dos programas simples: echo y true. Versiones simplificadas de los programas unix originales (issue #71).

---
+++ src/cppTestCode/simplified_unix_tools/echo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * Usage:
+ *    echo [msg]
+ *
+ * Prints to stdout the message 'msg' followed by a new line.
+ * If there isn't a 'msg', only a new line is printed.
+ * */ 
+int main(int argc, char *argv[]) {
+   if (argc == 1) {
+      printf("\n");
+   }
+   else if (argc == 2) {
+      printf("%s\n", argv[0]);
+   }
+   else {
+      return 1;
+   }
+
+   return 0;
+}
---
+++ src/cppTestCode/simplified_unix_tools/true.c
@@ -0,0 +1,6 @@
+/*
+ * Nothing, just returns.
+ * */
+int main(int argc, char *argv) {
+   return 0;
+}
