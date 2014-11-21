791be78a4b2583e256212b67c2355d49bba9b077
Di Paola Martin <martinp.dipaola@gmail.com>
2014-11-21 00:20:02 -0300

    Removido codigo de debugging y algunos codigos comentados.

--- src/py/gdb/Plugins/startAndBreak.py
+++ src/py/gdb/Plugins/startAndBreak.py
@@ -9,26 +9,20 @@ import gdb
 readWriteFlag = 2
 
 def redirectOutput(fifoPath):
-    print "redirigiendo a %s" % fifoPath
     gdb.execute('call open("%s", %i)' % (fifoPath, readWriteFlag), to_string=True)
     fdFifo = gdb.parse_and_eval("$")  # obtengo el ultimo valor de retorno
-    print  "fdFifo: %i" % fdFifo
+
     gdb.execute("call dup2( %i, %i)" % (fdFifo, 1))
-    dup2 = gdb.parse_and_eval("$")
-    print  "dup2: %i" % dup2
+
     gdb.execute("call setlinebuf(stdout)")
-    setlinebuf = gdb.parse_and_eval("$")
-    print  "setlinebuf: %i" % setlinebuf
     
 
 class StartAndBreak (gdb.Breakpoint):
         
     def register(self, path):
         self.path = path
-        print "registrado %s" % self.path
     
     def stop (self):
-        print ("redirecting")
         redirectOutput(self.path)
         return True
 
@@ -43,9 +37,7 @@ class CommandFifoRegister(gdb.Command):
 
     def invoke (self , args , from_tty) :            
         argv = gdb.string_to_argv(args)
-#         globalFifoPath = argv[0]
         breakpointStartAndBreak.register(argv[0])
-#         print "registrado %s" % globalFifoPath
 
 comandoFifoRegister = CommandFifoRegister()
 
