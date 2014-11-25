28ccca1fc010b1442d9334d805bfb24fc3ef82b5
Di Paola Martin <martinp.dipaola@gmail.com>
2014-11-24 23:19:25 -0300

    Implementada una vista para listas que pone en el DOM solo los elementos que se veran, el resto estan ocutos fuera del DOM. issue #32.

---
+++ src/js/listview.js
@@ -0,0 +1,171 @@
+define(["jquery"], function ($) {
+   /*
+                       the buffer (elements in the DOM)
+                      /---------------------------------\
+                     /                                   \
+                     | /-- buffer_position               |
+                     |/             view_height          |
+         0px         V        /--------------------/     V
+         .. .. .. .. |--------|--------------------|-----|.. .. .. .. ..
+         :  white    | hi     |                    |hi   |    white    :
+         :   top     |   dd   |      visible       | dd  |    bottom   :
+         :  space    |     en |                    |   en|    space    :
+         .. .. .. .. |--------|--------------------|-----|.. .. .. .. ..
+         \           \---------\                   \------\             \
+          \           top_buffer_height             bottom_buffer_height \
+           \--------------------------------------------------------------\
+                                    virtual_height
+   */
+         
+   var ListView = function () {
+      this.view_height = 300;
+      this.top_buffer_height = 200;
+      this.bottom_buffer_height = 200;
+
+      this.buffer_position = 0;
+      this.current_scroll_top = 0;
+      this.virtual_height = 0;
+
+      this.data = [];
+      this.dom_elements = [];
+
+      //var common_style = "border: 1px solid black; margin: 0px; padding: 0px;";
+      var common_style = "border: 0px; margin: 0px; padding: 0px;";
+      
+      this.$buffer = $('<div style="'+common_style+'"></div>');
+      this.$white_top_space = $('<div style="'+common_style+' height: 0px"></div>');
+      this.$white_bottom_space = $('<div style="'+common_style+' height: 0px"></div>');
+   };
+
+   ListView.prototype.attach = function (dom_element) {
+      if (this.$container) {
+         throw new Error("Already attached!");
+      }
+
+      this.$container = $(dom_element);
+      this.$white_top_space.appendTo(this.$container);
+      this.$buffer.appendTo(this.$container);
+      this.$white_bottom_space.appendTo(this.$container);
+
+      var self = this;
+      this.onScroll = function () { self.notify_scroll(); }
+      this.$container.on('scroll', this.onScroll);
+   };
+
+   ListView.prototype.detach = function () {
+      if (!this.$container) {
+         throw new Error("No attached!");
+      }
+
+      this.$container.off('scroll', this.onScroll);
+      this.$white_bottom_space.detach();
+      this.$buffer.detach();
+      this.$white_top_space.detach();
+
+      this.$container = this.onScroll = undefined;
+   };
+
+   ListView.prototype.append = function (dom_element, height) {
+      if (!height) {
+         height = $(dom_element).outerHeight(true);
+      }
+   
+      if (!height || height <= 0) {
+         throw new Error("Invalid height for the dom element "+dom_element+": "+height+"");
+      }
+
+      var position_of_new_element = this.virtual_height;
+
+      this.data.push({top: position_of_new_element});
+      this.dom_elements.push(dom_element);
+      this.virtual_height += height;
+
+      if (this._is_position_in_buffer(position_of_new_element)) {
+         this._update_buffer_and_white_space(this.current_scroll_top);
+      }
+      else {
+         this._update_white_space();
+      }
+
+   };
+
+   ListView.prototype._update_buffer = function (scrollTop) {
+      this.current_scroll_top = scrollTop;
+
+      var result = this._get_element_and_index(Math.max(scrollTop-this.top_buffer_height, 0), true);
+      
+      var roof_element       = result.element;
+      var roof_element_index = result.index;
+
+      var result = this._get_element_and_index(Math.min(scrollTop+this.view_height+this.bottom_buffer_height, this.virtual_height), false);
+
+      var floor_element       = result.element;
+      var floor_element_index = result.index;
+
+      this.buffer_position = roof_element.top;
+
+      var new_elements_in_buffer = this.dom_elements.slice(roof_element_index, floor_element_index+1);
+      this.$buffer.children().detach();
+      this.$buffer.append(new_elements_in_buffer);
+   };
+
+   ListView.prototype._update_white_space = function () {
+      this.$white_top_space.height(this.buffer_position);
+      this.$white_bottom_space.height(Math.max(this.virtual_height-this.current_scroll_top-this.view_height-this.bottom_buffer_height, 0));
+   };
+
+   ListView.prototype._update_buffer_and_white_space = function (scrollTop) {
+      this._update_buffer(scrollTop);
+      this._update_white_space();
+   };
+
+   ListView.prototype.notify_scroll = function () {
+      var new_scroll_top = this.$container.scrollTop();
+
+      if (this.buffer_position <= new_scroll_top && (new_scroll_top+this.view_height) < (Math.min(this.current_scroll_top+this.view_height+this.bottom_buffer_height, this.virtual_height))) {
+         // ok, still inside the buffer
+      }
+      else {
+         this._update_buffer_and_white_space(new_scroll_top);
+      }
+   };
+
+   ListView.prototype._is_position_in_buffer = function (position) {
+      return (this.buffer_position <= position && position <= (Math.min(this.current_scroll_top+this.view_height+this.bottom_buffer_height, this.virtual_height)));
+   };
+
+   ListView.prototype._get_element_and_index = function (position, round_to_roof) {
+      this.data.push({top: Number.POSITIVE_INFINITY});
+
+      var left = 0;
+      var right = this.data.length-2; // dont count the dummy node
+
+      while (left <= right) {
+         var middle = Math.floor((left+right) / 2);
+
+         var found = this.data[middle];
+         var next  = this.data[middle+1];
+
+         if (found.top <= position && position < next.top) {
+            this.data.pop();
+            if (!round_to_roof && middle < this.data.length) {
+               found = next;
+               middle = middle+1;
+            }
+            return {element: found, index: middle};
+         }
+
+         if (position < found.top) {
+            right = middle-1;
+         }
+         else {
+            left = middle+1;
+         }
+      }
+
+      this.data.pop();
+      throw new Error("None element was found in the position "+position+" (searched in "+this.data.length+" elements).");
+   };
+
+   return {ListView:ListView};
+});
