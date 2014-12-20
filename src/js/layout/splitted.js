cc0cf3dd36f99e9b5f04dba9a5f4451519111b99
Di Paola Martin <martinp.dipaola@gmail.com>
2014-12-20 01:37:08 -0300

    Dado que los splitteds pueden anidarse, aplicar un css a cada splitter hacia que sus efectos se acumularan (imaginarse un cuadrado dentro de otro). Ahora, solo se aplican dichos css solo si sus hijos no son contenedores.

--- src/js/layout/splitted.js
+++ src/js/layout/splitted.js
@@ -96,10 +96,10 @@ define(['jquery', 'layout/panel', 'jqueryui'], function ($, P, _) {
       if (this._splitted_direction === 'horizontally') {
          this._$container = $(
             '<div class="splitted_container ui-widget">' +
-               '<div class="top_panel_of_splitted ui-widget-content ui-corner-all"></div>' +
+               '<div class="top_panel_of_splitted"></div>' +
                '<div class="bottom_side_panel_and_bar_of_splitted">' +
                   '<div class="horizontal_bar_splitting ui-state-default bar_of_splitted_transparent"></div>' +
-                  '<div class="bottom_panel_of_splitted ui-widget-content ui-corner-all"></div>' +
+                  '<div class="bottom_panel_of_splitted"></div>' +
                '</div>' +
             '</div>');
          this._$container.find('.horizontal_bar_splitting').hover(function () {$(this).toggleClass("ui-state-hover").toggleClass("bar_of_splitted_transparent")}).draggable(options_for_draggable_bar('horizontal'));
@@ -109,10 +109,10 @@ define(['jquery', 'layout/panel', 'jqueryui'], function ($, P, _) {
       else {
          this._$container = $(
             '<div class="splitted_container ui-widget">' +
-               '<div class="left_panel_of_splitted ui-widget-content ui-corner-all"></div>' +
+               '<div class="left_panel_of_splitted"></div>' +
                '<div class="right_side_panel_and_bar_of_splitted">' +
                   '<div class="vertical_bar_splitting ui-state-default bar_of_splitted_transparent"></div>' +
-                  '<div class="right_panel_of_splitted ui-widget-content ui-corner-all"></div>' +
+                  '<div class="right_panel_of_splitted"></div>' +
                '</div>' +
             '</div>');
          this._$container.find('.vertical_bar_splitting').hover(function () {$(this).toggleClass("ui-state-hover").toggleClass("bar_of_splitted_transparent")}).draggable(options_for_draggable_bar('vertical'));
@@ -231,6 +231,14 @@ define(['jquery', 'layout/panel', 'jqueryui'], function ($, P, _) {
          else {
             this._children[pos].box = this._$container.children('.'+pos+'_side_panel_and_bar_of_splitted').children(this._get_class_for_child(pos));
          }
+
+         if (this._children[pos].is_container()) {
+            this._children[pos].box.removeClass("ui-widget-content").removeClass("ui-corner-all");
+         }
+         else {
+            this._children[pos].box.addClass("ui-widget-content").addClass("ui-corner-all");
+         }
+
          this._children[pos].render();
       }
    };
