257231988e5e3eebbe8119b10f0a48ab16e6f57d
NicolasDiTomaso <ditomasonicolas@gmail.com>
2014-12-20 21:23:43 -0300

    Ya se lanzan los eventos de manera correcta. El callback para el onChange esta puesto en la funcion de render, se que no esta bien ahi, aun no logre que me lo tome en otro lado.

---
+++ src/js/standarInput.js
@@ -0,0 +1,48 @@
+define([ 'event_handler', 'layout', 'jquery' ], function(event_handler, layout, $) {
+	
+	function StandarInput(){
+		this.super("STDIN");
+		
+		this.eventHandler = new event_handler.EventHandler();
+		this.eventHandler.init();
+		
+		var my_self = this;
+		
+		this.eventHandler = new event_handler.EventHandler();
+		this.eventHandler.init();
+
+		this._$container = $('<div id = recordSTDIN "></div><div id ="STDIN";"> pid@text:<input id="stdinText" type="text" value=""/> </div>');
+		
+	};
+	
+	
+	StandarInput.prototype.render = function () {
+		this._$rendered_in = $(this.box);
+		this._$rendered_in.html(this._$container);
+		
+		$('#stdinText').data("eventHandler", this.eventHandler);
+		
+		$('#stdinText').change(function(){//TODO SACAR ESTO DE ACA!!!!
+			var text = $(this).val();
+			$(this).val('');
+			console.log(text);
+			var separado = text.split('@');
+			$('#recordSTDIN').append(text + '</br>');
+			$(this).data("eventHandler").publish(separado[0] + ".stdin", separado[1]);
+		});
+	};
+
+	StandarInput.prototype.unlink = function () {
+		if (this._$rendered_in) {
+			this._$rendered_in.empty();
+			this._$rendered_in = null;
+		}
+	};
+
+	StandarInput.prototype.__proto__ = layout.Panel.prototype;
+	
+	
+	return {
+		StandarInput : StandarInput
+	};
+});
