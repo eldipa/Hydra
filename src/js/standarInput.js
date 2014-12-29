b582b687102967452b2b727efd6f8b71d534f3ff
NicolasDiTomaso <ditomasonicolas@gmail.com>
2014-12-29 20:17:58 -0300

    Arreglo provisional de bug que hacia que si no se ponia el '@' dentro del input, el sistema colgaba

--- src/js/standarInput.js
+++ src/js/standarInput.js
@@ -1,8 +1,9 @@
-define([ 'event_handler', 'layout', 'jquery' ], function(event_handler, layout, $) {
-	
-	function StandarInput(){
+define([ 'event_handler', 'layout', 'jquery' ], function(event_handler, layout,
+		$) {
+
+	function StandarInput() {
 		this.super("STDIN");
-		
+
 		this.eventHandler = new event_handler.EventHandler();
 		this.eventHandler.init();
 
@@ -10,44 +11,47 @@ define([ 'event_handler', 'layout', 'jquery' ], function(event_handler, layout, 
 		this.stdin = $('<div> pid@text: </div>');
 		this.stdin.append(this.input);
 		this.record = $('<div></div>');
-		
+
 		this._$container = $('<div></div>');
 		this._$container.append(this.record);
 		this._$container.append(this.stdin);
-		
+
 		this._$out_of_dom = this._$container;
-		
+
 		var my_self = this;
-		
-		$(document).ready(function () {
-			my_self.input.change(function(){
-				var text = $(this).val();
-				$(this).val('');
-				console.log(text);
-				var separado = text.split('@');
-				my_self.record.append(text + '</br>');
-				my_self.eventHandler.publish(separado[0] + ".stdin", separado[1]);
-			});
-		});
-	};
-	
-	
-	StandarInput.prototype.render = function () {
+
+		$(document).ready(
+				function() {
+					my_self.input.change(function() {
+						var text = $(this).val();
+						$(this).val('');
+						console.log(text);
+						var separado = text.split('@');
+						if (separado.length == 2) {
+							my_self.record.append(text + '</br>');
+							my_self.eventHandler.publish(
+									separado[0] + ".stdin", separado[1]);
+						}
+					});
+				});
+	}
+	;
+
+	StandarInput.prototype.render = function() {
 		if (this._$out_of_dom) {
-	         this._$out_of_dom.appendTo(this.box);
-	         this._$out_of_dom = null;
-	      }
+			this._$out_of_dom.appendTo(this.box);
+			this._$out_of_dom = null;
+		}
 	};
 
-	StandarInput.prototype.unlink = function () {
+	StandarInput.prototype.unlink = function() {
 		if (!this.$out_of_dom) {
-		     this.$out_of_dom = this._$container.detach();
-		  }
+			this.$out_of_dom = this._$container.detach();
+		}
 	};
 
 	StandarInput.prototype.__proto__ = layout.Panel.prototype;
-	
-	
+
 	return {
 		StandarInput : StandarInput
 	};
