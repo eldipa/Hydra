0fd2d8459aed28a91c88497e4dc3464e0b202aa9
NicolasDiTomaso <ditomasonicolas@gmail.com>
2015-10-18 04:18:23 -0300

    Testeado el comportamiento del redirect para un output

--- test_plugin/testSuite.py
+++ test_plugin/testSuite.py
@@ -21,13 +21,40 @@ class CompleteTest(unittest.TestCase):
     def test_stdioRedirect_load(self):
         gdb = self.manager.addManualGdb()
         self.assertTrue(int(gdb.get_gdb_pid()) > 0, "Pid de gdb erroneo")
+        request(gdb, "python import gdb_module_loader; gdb_module_loader.get_module_loader().load('stdioRedirect')", [])
+        request(gdb, "gdb-module-stdfd-redirect-activate", [])
         request(gdb, "-file-exec-and-symbols %s" % (testCodePath + "outputTest"))
+        # Ejecuto de forma normal, redirijo la salida a un archivo temporal para que no pise las salidas de gdb
+        request(gdb, "run > Salida.txt")
+        sleep(2)
+        #Compruebo la salida
+        archivoSalida = open("Salida.txt")
+        textoSalida = archivoSalida.read()
+        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "La salida normal no fue la esperada")
+        archivoSalida.close()
+        os.remove("Salida.txt")
         
-#         request(gdb, "-exec-run")
-#         sleep(1)
-        
-        
-        self.manager.printEvents()
+        #Realizo la misma operacion, pero luego de iniciada la ejecucion, redirijo a otro path
+        request(gdb, "start > Salida.txt")
+        sleep(1)
+        open("NuevaSalida.txt", 'a')
+        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["1","NuevaSalida.txt"])
+        request(gdb, "continue")
+        sleep(2)
+        #Compruebo la nueva salida
+        archivoSalida = open("NuevaSalida.txt")
+        textoSalida = archivoSalida.read()
+        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "La salida nueva no fue la esperada")
+        archivoSalida.close()
+        os.remove("NuevaSalida.txt")
+        #Compruebo que no haya texto en la salida original
+        archivoSalida = open("Salida.txt")
+        textoSalida = archivoSalida.read()
+        self.assertEqual(textoSalida, "", "La salida vieja no fue la esperada")
+        archivoSalida.close()
+        os.remove("Salida.txt")
+
+#         self.manager.printEvents()
 
         
 #     def test_stdioRedirect_attach(self):
