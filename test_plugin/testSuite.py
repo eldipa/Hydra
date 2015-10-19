7c890fd0e16a9dd4b46d41310ab14b5153e23d49
NicolasDiTomaso <ditomasonicolas@gmail.com>
2015-10-19 03:48:14 -0300

    agrego test para redireccionar stdout luego de un attach

--- test_plugin/testSuite.py
+++ test_plugin/testSuite.py
@@ -1,14 +1,13 @@
 import unittest
 import os
-import time 
 import sys
-import pprint
-import select
 from time import sleep
 sys.path.append("../src/py")
 sys.path.append("../src/ipc/pyipc")
 import gdbManager
 from shortcuts import request
+import subprocess
+from subprocess import PIPE
 
 testCodePath = "./cppTestCode/"
 
@@ -33,7 +32,7 @@ class CompleteTest(unittest.TestCase):
         # Compruebo la salida
         archivoSalida = open("Salida.txt")
         textoSalida = archivoSalida.read()
-        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "La salida normal no fue la esperada")
+        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "Se recibio: " + textoSalida)
         archivoSalida.close()
         os.remove("Salida.txt")
         
@@ -41,25 +40,25 @@ class CompleteTest(unittest.TestCase):
         request(gdb, "start > Salida.txt")
         sleep(1)
         open("NuevaSalida.txt", 'a').close()
-        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["1", "NuevaSalida.txt"])
+        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["1", "NuevaSalida.txt", "1"])
         request(gdb, "continue")
         sleep(2)
         
         # Compruebo la nueva salida
         archivoSalida = open("NuevaSalida.txt")
         textoSalida = archivoSalida.read()
-        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "La salida nueva no fue la esperada")
+        self.assertEqual(textoSalida, "Linea numero 0\nLinea numero 1\nLinea numero 2\nLinea numero 3\nLinea numero 4\n", "Se recibio: " + textoSalida)
         archivoSalida.close()
         os.remove("NuevaSalida.txt")
         
         # Compruebo que no haya texto en la salida original
         archivoSalida = open("Salida.txt")
         textoSalida = archivoSalida.read()
-        self.assertEqual(textoSalida, "", "La salida vieja no fue la esperada")
+        self.assertEqual(textoSalida, "", "Se recibio: " + textoSalida)
         archivoSalida.close()
         os.remove("Salida.txt")
 
-        
+    @unittest.skipIf(True, "Manual Skip")    
     def test_stdioRedirect_input_on_load(self):
         gdb = self.manager.addManualGdb()
         self.assertTrue(int(gdb.get_gdb_pid()) > 0, "Pid de gdb erroneo")
@@ -78,7 +77,7 @@ class CompleteTest(unittest.TestCase):
         # Compruebo la salida
         archivoSalida = open("Salida.txt")
         textoSalida = archivoSalida.read()
-        self.assertEqual(textoSalida, "Ingrese un texto:\nUsted ingreso: Hola\nIngrese un texto:\nFin\n", "La salida no fue la esperada")
+        self.assertEqual(textoSalida, "Ingrese un texto:\nUsted ingreso: Hola\nIngrese un texto:\nFin\n", "Se recibio: " + textoSalida)
         archivoSalida.close()
         os.remove("Salida.txt")
         
@@ -98,7 +97,7 @@ class CompleteTest(unittest.TestCase):
         entrada.write("Esta es la entrada desde una archivo\nTiene 2 lineas en total\n")
         entrada.close()
         
-        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["0", "Entrada.txt"])
+        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["0", "Entrada.txt", "0"])
         request(gdb, "continue")
         sleep(1)
         gdb.gdbInput.close()  # Cierro la entrada para que sea tomada como un EOF
@@ -109,10 +108,50 @@ class CompleteTest(unittest.TestCase):
         # Compruebo la salida
         archivoSalida = open("Salida.txt")
         textoSalida = archivoSalida.read()
-        self.assertEqual(textoSalida, "Ingrese un texto:\nUsted ingreso: Esta es la entrada desde una archivo\nIngrese un texto:\nUsted ingreso: Tiene 2 lineas en total\nIngrese un texto:\nFin\n", "La salida no fue la esperada")
+        self.assertEqual(textoSalida, "Ingrese un texto:\nUsted ingreso: Esta es la entrada desde una archivo\nIngrese un texto:\nUsted ingreso: Tiene 2 lineas en total\nIngrese un texto:\nFin\n", "Se recibio: " + textoSalida)
         archivoSalida.close()
         os.remove("Salida.txt")
-    
+        
+    @unittest.skipIf(False, "Manual Skip")
+    def test_stdioRedirect_output_on_attach(self):
+        #Creo un gdb vacio, e incio el modulo de redireccion
+        gdb = self.manager.addManualGdb()
+        self.assertTrue(int(gdb.get_gdb_pid()) > 0, "Pid de gdb erroneo")
+        request(gdb, "python import gdb_module_loader; gdb_module_loader.get_module_loader().load('stdioRedirect')", [])
+        request(gdb, "gdb-module-stdfd-redirect-activate", [])
+        
+        #Creo un proceso nuevo al cual voy a attacharme
+        target = subprocess.Popen([testCodePath + "stdinTest"], stdin=PIPE, 
+                        stdout=PIPE, stderr=open('/dev/null', 'w'))
+        #Simple prueba de que el programa se esta ejecutando
+        os.write(target.stdin.fileno(), "Sin Dirigir\n")
+        sleep(1)
+        textoSalida = os.read(target.stdout.fileno(), 1024)
+        self.assertEqual(textoSalida, "Ingrese un texto:\nUsted ingreso: Sin Dirigir\nIngrese un texto:\n", "Se recibio: " + textoSalida)
+        
+        #Realizo el attach y hago el redireccionamiento de stdout a NuevaSalida.txt
+        request(gdb, "-target-attach", [str(target.pid)])
+        open("NuevaSalida.txt", 'a').close()
+        request(gdb, "gdb-module-stdfd-redirect-redirect_target_to_destine_file", ["1", "NuevaSalida.txt", "1"])
+        request(gdb, "continue")
+        sleep(2)
+
+        os.write(target.stdin.fileno(), "Primera linea a ser redireccionada\n")
+        sleep(1)
+
+        # Compruebo la nueva salida
+        archivoSalida = open("NuevaSalida.txt")
+        textoSalida = archivoSalida.read()
+        self.assertEqual(textoSalida, "Usted ingreso: Primera linea a ser redireccionada\nIngrese un texto:\n", "Se recibio: " + textoSalida )
+        archivoSalida.close()
+        os.remove("NuevaSalida.txt")
+        sleep(2)
+        
+        target.stdin.close()
+        target.wait()
+
+        
+        
     def tearDown(self):
         self.manager.close()    
     
