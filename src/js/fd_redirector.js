298abe1f773ef4332b97ff8c050d79c5a99a1e81
NicolasDiTomaso <ditomasonicolas@gmail.com>
2015-11-04 05:53:54 -0300

    cambios minimos hechos para lograr que sigma funcione. sin exitos de momento

--- src/js/fd_redirector.js
+++ src/js/fd_redirector.js
@@ -4,7 +4,7 @@ define([ 'jquery', 'layout', 'shortcuts', 'sigma' ], function($, layout,
 	function FD_Redirector() {
 		this.super("FD Redirector")
 
-		this._$container = $('<style>#graph-container {top: 0;bottom: 0;left: 0;right: 0;position: absolute;}</style><div id="graph-container"></div>');
+		this._$container = $('<div id="sig" style="top: 0;bottom: 0;left: 0;right: 0;position: absolute;"> No anda</div>');
 
 		this._$out_of_dom = this._$container;
 
@@ -30,25 +30,30 @@ define([ 'jquery', 'layout', 'shortcuts', 'sigma' ], function($, layout,
 				source : 'n' + (Math.random() * N | 0),
 				target : 'n' + (Math.random() * N | 0),
 				size : Math.random(),
-				color : '#ccc'
+				color : '#fff'
 			});
 
+	
 		// Instantiate sigma:
-		s = new sigma({
+
+		sigmaGraph = new sigma({
 			graph : g,
-			container : this._$container
+			container : this._$container // 'sig' no parece funcionar tampoco
 		});
+
+		
+		
 	};
 
 	FD_Redirector.prototype.__proto__ = layout.Panel.prototype;
 
 	FD_Redirector.prototype.render = function() {
 		if (this._$out_of_dom) {
-			if ($(this.box).css('position') === 'relative') {
-				this._$out_of_dom.css('position', 'absolute');
-			} else {
-				this._$out_of_dom.css('position', '');
-			}
+//			if ($(this.box).css('position') === 'relative') {
+//				this._$out_of_dom.css('position', 'absolute');
+//			} else {
+//				this._$out_of_dom.css('position', '');
+//			}
 
 			this._$out_of_dom.appendTo(this.box);
 			this._$out_of_dom = null;
