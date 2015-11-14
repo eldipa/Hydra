c484e8e36c7744f5557e5a42d000eb67b3757109
Di Paola Martin <martinp.dipaola@gmail.com>
2015-11-14 15:57:18 -0300

    Refactorizado el creador de snippets.

--- src/js/snippet.js
+++ src/js/snippet.js
@@ -1,5 +1,5 @@
 define(['ace', 'jquery', 'underscore'], function (ace, $, _) {
-    var create_snippet = function (code, is_assembly) {
+    var create_snippet = function (code, when_ready_callback, opts) {
         var font_size = 11;
         var height = font_size + 3; //px
 
@@ -15,24 +15,29 @@ define(['ace', 'jquery', 'underscore'], function (ace, $, _) {
         editor.setHighlightActiveLine(false);
         editor.renderer.setShowGutter(false);
         
-        var ace_mode = is_assembly? "ace/mode/assembly_x86" : "ace/mode/c_cpp";
+        var ace_mode = opts.is_assembly? "ace/mode/assembly_x86" : "ace/mode/c_cpp";
         editor.getSession().setMode(ace_mode);
 
         editor.setValue(code);
 
-        $('body').append(_$container);
-        //editor.resize(true);
-        var renderer = editor.renderer;
-        renderer.$renderChanges(2047, true);
+        $(opts.temporal_in_dom_element || 'body').append(_$container);
 
-        //_$container = _$container.detach();
+        setTimeout(function() {
+                var container = $(_$container);
+                container.children(":not(.ace_scroller)").remove();
 
-        console.log(editor);
-        console.log(editor.getSession());
+                var scroller = container.children(".ace_scroller");
+                scroller.children(":not(.ace_content)").remove();
 
-        //var line = _$container.find(".ace_line"); //TODO how to update the DOM stuff so we can borrow the ace_line???
-        //var line = _$container.find(".ace_text-layer");
-        return _$container; //line;
+                var content = scroller.children(".ace_content");
+                content.children(":not(.ace_text-layer)").remove();
+
+                var cloned_without_event_handlers = container.clone(false, false);
+                container.remove();
+
+                when_ready_callback(cloned_without_event_handlers);
+          }, 300); // TODO this is hardcoded!! we dont know how to build the snippet with ace synchronously.
+                   // Our best shot is to wait some time and then we put the snippet into its final target.
     };
 
     return {create_snippet: create_snippet};
