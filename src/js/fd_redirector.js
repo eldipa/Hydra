6b31c1b7884eb523c158540c25361a1c00bb5f53
Nicolas Di Tomaso <ditomasonicolas@gmail.com>
2015-11-19 04:06:31 -0300

    Llamo al plugin de redireccion. De momento no anda, creo que no se esta cargando el plugin al inicio.

--- src/js/fd_redirector.js
+++ src/js/fd_redirector.js
@@ -1,6 +1,6 @@
 define(
-		[ 'jquery', 'layout', 'shortcuts', 'springy', 'springyui' ],
-		function($, layout, shortcuts, Springy, springyui) {
+		[ 'jquery', 'layout', 'shortcuts', 'springy', 'springyui', 'event_handler' ],
+		function($, layout, shortcuts, Springy, springyui, event_handler) {
 
 			function FD_Redirector() {
 				this.super("FD Redirector")
@@ -8,6 +8,8 @@ define(
 				this._$container = $('<canvas width="800" height="400"> Error al cargar Springy </canvas>');
 
 				this._$out_of_dom = this._$container;
+				
+				this.EH = event_handler.get_global_event_handler();
 
 				// make a new graph
 				this.graph = new Springy.Graph();
@@ -15,7 +17,7 @@ define(
 				this.selectedNodeInfo = null;
 
 				this.pid = 0;
-				this.gdb = 0;
+				this.debugger_id = 0;
 
 				var my_self = this;
 
@@ -55,10 +57,24 @@ define(
 									var input_file_dom = $('<input style="display:none;" type="file" />');
 									input_file_dom.change(function(evt) {
 										var file_path = "" + $(this).val();
+										 // Saco la ultima letra (modo) para que quede solo el numero del FD
+										var fd = my_self.selectedNodeInfo["FD"].slice(0, -1);
+										
+										//Vero el modo de apertura del archivo
+										var modos_posibles = {"r" : 0, "w" : 1 , "u" : 2}; 
+										var modo = modos_posibles[my_self.selectedNodeInfo["FD"].slice(-1)];
+										
+										console.log(fd);
+										console.log(modo);
 										if (file_path) {
 											console.log(file_path);
+											shortcuts.gdb_request(my_self.refresh, 
+									                this.debugger_id, 
+									                "gdb-module-stdfd-redirect-redirect_target_to_destine_file",
+									                [fd, file_path, modo]
+									                );
 										} else {
-											console.log("Loading nothing");
+											console.log("Wont Redirect");
 										}
 									});
 									input_file_dom.trigger('click');
@@ -74,7 +90,7 @@ define(
 
 			FD_Redirector.prototype.follow = function(thread) {
 				this.pid = thread.get_thread_group_you_belong().process_id;
-				this.gdb = thread.get_thread_group_you_belong().debugger_id;
+				this.debugger_id = thread.get_thread_group_you_belong().debugger_id;
 			}
 
 			FD_Redirector.prototype.refreshGraph = function(DataOfFD) {
