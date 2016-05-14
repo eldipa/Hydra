7dd6731fc7dd78d031bf79b0be128fc9fae05a7e
Di Paola Martin <martinp.dipaola@gmail.com>
2016-05-13 23:27:38 -0300

    Refactorizacion del LogView para darle soporte a un esquema mas flexible para renderizar distintas lineas de log. Por ahora es experimental.

--- src/js/log_view.js
+++ src/js/log_view.js
@@ -103,16 +103,110 @@ define(["underscore", "jquery", "layout", "slickgrid", "event_handler", "observa
    };
    
    LogView.prototype._do_observation_for_this_row = function (row_index) {
-       var message = this.data[row_index].message;
-       var message_object = {
-            get_display_name: function () { return "XXX display name"; },
-            get_display_details: function () { return $("<span></span>").text(message); },
-            get_display_fullname: function () { return "XXX display fullname"; },
-            get_display_controller: function () { return null; },
-       };
-
+       var message_object_constructor = this.data[row_index].message_object_constructor || Message;
+       var message_object = new message_object_constructor(row_index, this.data[row_index]);
        return new Observation({target: message_object, context: this});
    };
 
+   /* All the lines by default will construct this kind of objects when they are clicked (see do_observation_over_the_grid)
+    * If you want other kind of object, add the property 'message_object_constructor' in the line stored in the grid,
+    * the method _do_observation_for_this_row will use that function to build your custom message.
+    * */
+   var Message = function (row_index, log_line_object) {
+       this.raw_message = log_line_object.message;
+       this.formatted_message = this.raw_message;
+   }
+
+   Message.prototype.get_display_name = function () {
+       return "A message";
+   };
+   
+   Message.prototype.get_display_details = function () {
+        return $("<pre></pre>").html(this.formatted_message);
+   };
+   
+   Message.prototype.get_display_fullname = function () {
+       return "A message";
+   };
+   
+   Message.prototype.get_display_controller = function () {
+       return null;
+   };
+
+   Message.prototype.get_display_controller = function (current_context) {
+       var self = this;
+
+       var _is_the_details_view = current_context._is_the_details_view || false;
+
+       if (!_is_the_details_view) {
+           return [];
+       }
+
+       var _details_view = current_context;
+       var controller = [   
+            {
+                text: "Show as...",
+                subMenu: [
+                    {
+                        text: "text",
+                        action: function (e) {
+                           e.preventDefault();
+                           self.formatted_message = self.raw_message;
+                            
+                           _details_view.update_view();
+                        }
+                    },
+                    {
+                        text: "json",
+                        action: function (e) {
+                           e.preventDefault();
+                           try {
+                               var obj = JSON.parse(self.raw_message);
+                               var indented = JSON.stringify(obj, null, 2);
+                              
+                               self.formatted_message = syntaxHighlight(indented);
+                               _details_view.update_view();
+                           }
+                           catch (e) {
+                               self.formatted_message = self.raw_message;
+                               _details_view.update_view();
+                               throw e;
+                           }
+                        }
+                    }
+                ]
+            },
+       ];
+       
+       return controller;
+   };
+
+   // from https://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+   function syntaxHighlight(json) {
+    var css_style = {
+        string_  : "color: green;",
+        number_  : "color: darkorange;",
+        boolean_ : "color: blue;",
+        null_    : "color: magenta;",
+        key_     : "color: red;",
+    };
+
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number_';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key_';
+            } else {
+                cls = 'string_';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean_';
+        } else if (/null/.test(match)) {
+            cls = 'null_';
+        }
+        return '<span style="' + css_style[cls] + '">' + match + '</span>';
+    });
+   }
    return { LogView: LogView };
 });
