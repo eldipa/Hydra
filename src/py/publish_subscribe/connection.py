3199c3ffe8f2c557fe60bbb192dee4989cca030a
Martin Di Paola <martinp.dipaola@gmail.com>
2016-10-26 09:46:26 -0300

    Arreglado un bug en javascript que hacia crashear todo por un buffer underflow cuando la data a enviar tenia un caracter unicode que requeria mas de un byte para representarse.

--- src/py/publish_subscribe/connection.py
+++ src/py/publish_subscribe/connection.py
@@ -8,11 +8,20 @@ import traceback
 from esc import esc
 from message import unpack_message_header
 
+class ConnectionClosed(Exception):
+    def __init__(self, msg=""):
+        Exception.__init__(self, "The connection was closed between messages (no message was sent/recieved partially). " + msg)
+
+class PartialMessageDueConnectionClose(Exception):
+    def __init__(self, msg=""):
+        Exception.__init__(self, "The message was sent/recieved partially due an unexpected connection close. " + msg)
+
 class Connection(object):
-   def __init__(self, address_or_already_open_socket):
+   def __init__(self, address_or_already_open_socket, whoiam="(?)"):
       self.buf = ""
       self.end_of_the_communication = False
       self.closed = True
+      self.whoiam = whoiam
 
       if isinstance(address_or_already_open_socket, (tuple, list)):
          address = address_or_already_open_socket
@@ -42,7 +51,7 @@ class Connection(object):
          raise Exception("The communication is already close")
 
       #syslog.syslog(syslog.LOG_DEBUG, 
-      #        " ".join(["sending...", 
+      #        " ".join(["%s is sending..." % self.whoiam, 
       #        str(len(message)), 
       #        repr(message), 
       #        ]))
@@ -56,9 +65,9 @@ class Connection(object):
       header = self._read_next_message_header()
       message_type, message_body = self._read_next_message_body(header)
       #syslog.syslog(syslog.LOG_DEBUG, 
-      #        " ".join(["received", 
+      #        " ".join(["%s received" % self.whoiam, 
       #        str(len(header)), 
-      #        repr(header), 
+      #       repr(header), 
       #        "op:", 
       #        message_type, 
       #        str(len(message_body)), 
@@ -99,7 +108,10 @@ class Connection(object):
 
       if to_receive > 0:
           self.end_of_the_communication = True
-          raise Exception("End of the communication.")
+          if to_receive == 3:
+              raise ConnectionClosed()
+          else:
+              raise PartialMessageDueConnectionClose("Recieved %i bytes of the header" % (3-to_receive))
 
       return header
 
@@ -119,7 +131,7 @@ class Connection(object):
 
       if to_receive > 0:
           self.end_of_the_communication = True
-          raise Exception()
+          raise PartialMessageDueConnectionClose("Recieved %i bytes of the message body" % (message_body_len-to_receive))
 
       return message_type, message_body
 
