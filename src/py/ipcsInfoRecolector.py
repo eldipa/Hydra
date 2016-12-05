b4da534c053b8f177626be60a89375123cbf1eb8
nicolas <ditomasonicolas@gmail.com>
2016-12-05 20:11:00 -0300

    Parseo de toda la info de semaforos dada por ipcs -s. De momento solo se imprime por pantalla. Agregado simple codigo cpp para crear y borrar semaforos.

--- src/py/ipcsInfoRecolector.py
+++ src/py/ipcsInfoRecolector.py
@@ -21,45 +21,98 @@ class IPCSInfoRecolector(threading.Thread):
         self.processInfo = []
         while not self.salir:
             
-            shmemIPCS = subprocess.check_output(["ipcs","-m"]).splitlines()
+            print self.getShmemInfo()
             
-            #key, shmid, propietario, perms, bytes, nattch, estado
-            headers = shmemIPCS[2].split()
-            
-            shmemInfo = []
-            
-            #basci info
-            for i in range(3,len(shmemIPCS) - 1):
-                info={}
-                values = shmemIPCS[i].split()
-                for j in range(len(headers)):
-                    info.update({headers[j]: values[j]})
-                shmemInfo.append(info)
-                
-            #detailed info
-            for shmem in shmemInfo:
-                info = {}
-                detailedInfo = subprocess.check_output(["ipcs","-m", "-i", shmem["shmid"]]).splitlines()
-                
-                info.update(self.splitDetailedInfo(detailedInfo[2].split()))
-
-                info.update(self.splitDetailedInfo(detailedInfo[3].split()))
-
-                info.update(self.splitDetailedInfo(detailedInfo[4].split()))
-
-                info.update(self.splitDetailedInfo([detailedInfo[5]]))
-                
-                shmem.update(info)
-                
-            
-            print shmemInfo
+            print self.getSemInfo()
             
             sleep(self.recolectionInterval)
+            
+    def getShmemInfo(self):
+        shmemIPCS = subprocess.check_output(["ipcs","-m"]).splitlines()
+        
+        #key, shmid, propietario, perms, bytes, nattch, estado
+        headers = shmemIPCS[2].split()
+        
+        shmemInfo = []
+        
+        #basci info
+        for i in range(3,len(shmemIPCS) - 1):
+            info={}
+            values = shmemIPCS[i].split()
+            for j in range(len(headers)):
+                info.update({headers[j]: values[j]})
+            shmemInfo.append(info)
+            
+        #detailed info
+        for shmem in shmemInfo:
+            info = {}
+            detailedInfo = subprocess.check_output(["ipcs","-m", "-i", shmem["shmid"]]).splitlines()
+            
+            info.update(self.splitDetailedInfo(detailedInfo[2].split()))
+
+            info.update(self.splitDetailedInfo(detailedInfo[3].split()))
+
+            info.update(self.splitDetailedInfo(detailedInfo[4].split()))
+
+            info.update(self.splitDetailedInfo([detailedInfo[5]]))
+            
+            shmem.update(info)
+            
+        
+        return shmemInfo
+    
+    def getSemInfo(self):
+        semIPCS = subprocess.check_output(["ipcs","-s"]).splitlines()
+        
+        #'key', 'semid', 'propietario', 'perms', 'nsems'
+        headers = semIPCS[2].split()
+        
+        semInfo = []
+        
+        #basci info
+        for i in range(3,len(semIPCS) - 1):
+            info={}
+            values = semIPCS[i].split()
+            for j in range(len(headers)):
+                info.update({headers[j]: values[j]})
+            semInfo.append(info)
+            
+        
+        #detailed info
+        for sem in semInfo:
+            info = {}
+            detailedInfo = subprocess.check_output(["ipcs","-s", "-i", sem["semid"]]).splitlines()
+            
+            info.update(self.splitDetailedInfo(detailedInfo[2].split()))
+
+            info.update(self.splitDetailedInfo(detailedInfo[3].split()))
+
+            info.update(self.splitDetailedInfo([detailedInfo[4]]))
+
+            info.update(self.splitDetailedInfo([detailedInfo[5]]))
+            
+            info.update(self.splitDetailedInfo([detailedInfo[6]]))
+            
+            subHeaders = detailedInfo[7].split()
+            
+            subinfo = []
+            for i in range(int(info["nsems"])):
+                individualInfo = {}
+                singularSemData = detailedInfo[8 + i].split()
+                for j in range(len(subHeaders)):
+                    individualInfo.update({subHeaders[j]: singularSemData[j]})
+                subinfo.append(individualInfo)
+            
+            info.update({"sems": subinfo})
+            
+            sem.update(info)
+        
+        return semInfo
     
     def splitDetailedInfo(self, infoArray):
         aux = {}
         for item in infoArray:
             item = item.split("=")
-            aux.update({item[0]:item[1]})
+            aux.update({item[0].strip():item[1].strip()})
         return aux
         
